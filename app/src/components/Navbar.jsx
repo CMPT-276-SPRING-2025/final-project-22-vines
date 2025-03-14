32da567b3d5f127bb9b44b856aa9007c47d1f444
Elton <khoa.dhaminh723@gmail.com>
2025-03-14 16:29:27 -0700

    Fix minor typo in the project name

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-black'>
-        <h1 className='w-full text-3xl font-bold'><Link to="/">Digital Garden</Link></h1>
+        <h1 className='w-full text-3xl font-bold'><Link to="/">Virtual Garden</Link></h1>
         <ul className='flex' >
             <li className='p-4'>About</li>
             <li className='p-4'>FAQ</li>
